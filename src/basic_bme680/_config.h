3c81eae51fb327f85c1bd6391054d4959f53e52b
Nat <nat.wrw@gmail.com>
2020-04-30 23:09:03 +0700

    initial commit

---
+++ src/basic_bme680/_config.h
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+/* BOARD INFO */
+String DEVICE_NAME      = "BME680-01";
+
+/* WIFI INFO */
+String WIFI_SSID        = "CMMC_Sinet_2.4G";
+String WIFI_PASSWORD    = "zxc12345";
+
+/* MQTT INFO */
+String MQTT_HOST        = "mqtt.cmmc.io";
+String MQTT_USERNAME    = "";
+String MQTT_PASSWORD    = "";
+String MQTT_CLIENT_ID   = "";
+String MQTT_PREFIX      = "CMMC/NAT/";
+
+int    MQTT_PORT        = 1883;
+int PUBLISH_EVERY       = 10L * 1000;
+int MQTT_CONNECT_TIMEOUT= 10;
